7213c1b019f1e27e7276c3bb9d6991fba904e98e
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2020-06-17 23:59:20 -0300

    Update optional_param.sh

--- bash/optional_param.sh
+++ bash/optional_param.sh
@@ -1,5 +1,5 @@
 # Run this as `./optional-param.sh param1 param2` to see running with two params and then 
-if [ ! -z $2 ] 
+if [ ! -z $2 ]  # not empty string https://stackoverflow.com/a/6482403/1273751 comment from gcb Feb 26 '14
 then
    echo "print two params " $1 $2
 else
