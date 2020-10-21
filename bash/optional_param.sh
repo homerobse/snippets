1a2736e511c2f47942e80facf4622c3ef98f1cb9
Homero Esmeraldo <homero.esmeraldo@gmail.com>
2020-10-21 00:48:13 -0300

    Adding argument manipulation bash examples

--- bash/optional_param.sh
+++ bash/optional_param.sh
@@ -1,4 +1,4 @@
-# Run this as `./optional-param.sh param1 param2` to see running with two params and then 
+# Run this as `./optional-param.sh param1 param2` to see running with two params and then run with only param1
 if [ ! -z $2 ]  # not empty string https://stackoverflow.com/a/6482403/1273751 comment from gcb Feb 26 '14
 then
    echo "print two params " $1 $2
